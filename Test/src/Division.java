4af7f23ea277370e5b014ac2cf12c941d1e2c60b
FoxyCorndog <BradenSteffaniak@gmail.com>
2012-11-09 10:21:44 -0600

    ASDFASDFASDF

--- Test/src/Division.java
+++ Test/src/Division.java
@@ -6,26 +6,38 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+/**
+* File:  			Division.java
+* Author:			Braden Steffaniak
+* Programming:  	APCS
+* Last Modified: 	8Nov2012
+* Description: Shows off a few AWESOME math methods.
+*/
+
 public class Division
 {
 	int variables[];
 	
+	/**
+	 * The entry point of the program in which the program starts.
+	 * Runs the functions from the Division class.
+	 * 
+	 * @param args The command line arguments.
+	 */
 	public static void main(String args[])
 	{
 		Division d = new Division();
 		
-		
-	}
-	
-	public Division()
-	{
-//		write(40, 6, 7, 73);
 		try
 		{
-			read();
+			d.read();
 			
-			System.out.println(gcf());
-			System.out.println(lcm());
+			System.out.println("Read the variables: " + d.variables[0] +
+					", " + d.variables[1] + ", " + d.variables[2] +
+					", " + d.variables[3]);
+			
+			System.out.println("GCF of first two: " + d.gcf());
+			System.out.println("LCM of last two: " + d.lcm());
 		}
 		catch (IOException e)
 		{
@@ -33,6 +45,12 @@ public class Division
 		}
 	}
 	
+	/**
+	 * Method used to find the greatest common factor of the
+	 * first two integers read from the file.
+	 * 
+	 * @return The greatest common factor.
+	 */
 	private int gcf()
 	{
 		int max = Math.max(variables[0], variables[1]);
@@ -49,14 +67,18 @@ public class Division
 		return 1;
 	}
 	
+	/**
+	 * Method used to find the least common multiple of the
+	 * last two integers read from the file.
+	 * 
+	 * @return The least common multiple.
+	 */
 	private int lcm()
 	{
 		int max = Math.max(variables[2], variables[3]);
 		int min = Math.min(variables[2], variables[3]);
 		
-//		for ()
-		
-		for (int i = max;; i += min)
+		for (int i = max;; i += max)
 		{
 			if (i % max == 0 && i % min == 0)
 			{
@@ -65,9 +87,16 @@ public class Division
 		}
 	}
 	
+	/**
+	 * Method that reads integers from a file and stores them
+	 * in the variables array.
+	 * 
+	 * @throws IOException
+	 */
 	private void read() throws IOException
 	{
-		BufferedReader br = new BufferedReader(new FileReader(new File("data.txt")));
+		BufferedReader br = new BufferedReader(new FileReader(
+				new File("data.txt")));
 		
 		String line = br.readLine();
 		
