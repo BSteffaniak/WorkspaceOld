d2fd5d6e90e3c47367917bac037bafd7e1b36ed1
FoxyCorndog <BradenSteffaniak@gmail.com>
2013-04-15 06:47:13 -0500

    Phone updates

--- GridWorldExample/src/ChameleonKid.java
+++ GridWorldExample/src/ChameleonKid.java
@@ -6,5 +6,44 @@
  */
 public class ChameleonKid extends ChameleonCritter
 {
-	
+	public void makeMove()
+	{
+		super.makeMove()
+		
+		Grid grid = getGrid();
+		
+		ArrayList<Location> occupied = grid.getOccupiedAdjacentLocations();
+		
+		if (occupied.size() > 0)
+		{
+			
+		}
+		else
+		{
+			Color col = getColor();
+			
+			int r = col.getR();
+			int g = col.getG();
+			int b = col.getB();
+			
+			int amount = 5;
+			
+			r -= amount;
+			g -= amount;
+			b -= amount;
+			
+			if (r < 0)
+				r = 0;
+
+			if (g < 0)
+				g = 0;
+
+			if (b < 0)
+				b = 0;
+			
+			Color newCol = newColor(r, g, b);
+			
+			setColor(newCol);
+		}
+	}
 }
